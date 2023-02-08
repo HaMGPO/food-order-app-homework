f074863d7580ae2514d8819904a36d131f5e5dd0
sadabjr <sadabjr047@gmail.com>
2023-02-08 18:46:42 +0530

    updated

--- src/app/components/pages/food-page/food-page.component.ts
+++ src/app/components/pages/food-page/food-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
+import { food } from 'src/app/shared/model/Food';
+
 
 @Component({
   selector: 'app-food-page',
   templateUrl: './food-page.component.html',
   styleUrls: ['./food-page.component.css']
 })
-export class FoodPageComponent {
+export class FoodPageComponent implements OnInit{
+  food!:food;
+  constructor(activatedRoute:ActivatedRoute, private api:FoodService){
+    activatedRoute.params.subscribe((params) => {
+      if(params.id)
+      this.food = api.getFoodById(params.id)
+    })
+  }
+  ngOnInit(): void {
 
+  }
 }
